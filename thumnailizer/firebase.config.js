c904eafcd992634584efc26ee248cf2da645ab62
Eric-Luna <luna.eric.santiago@gmail.com>
2021-04-22 21:50:33 -0300

    Rollback envs

--- thumnailizer/firebase.config.js
+++ thumnailizer/firebase.config.js
@@ -6,13 +6,13 @@ import 'firebase/storage';
 console.log({env: process.env})
 
 const config = {
-  apiKey: process.env.apiKey,
-  authDomain: process.env.authDomain,
-  projectId: process.env.projectId,
-  storageBucket: process.env.storageBucket,
-  messagingSenderId: process.env.messagingSenderId,
-  appId: process.env.appId,
-  measurementId: process.env.measurementId
+  apiKey: "AIzaSyCt7gbz5zRKBTL6dt2PI-vp34iowkjt0qU",
+  authDomain: "imgrszr-dc5dd.firebaseapp.com",
+  projectId: "imgrszr-dc5dd",
+  storageBucket: "imgrszr-dc5dd.appspot.com",
+  messagingSenderId: "31675348623",
+  appId: "1:31675348623:web:6e10abe4183dab3dd022fa",
+  measurementId: "G-3QF1V1SFJ0"
 }
 
 try {
